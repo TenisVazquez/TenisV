1ac55708f156af2346082587f7b0bc5cc25b5fbe
TenisVazquez <mvke2225i@gmail.com>
2025-06-03 17:45:01 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+var reply = prompt("Bienvenid@ ¿cuál es tu nombre?", "")
+alert ( "Es un gusto tenerte aqui " + reply + "!")
+
+let miBoton = document.querySelector('button');
+miBoton.onclick = function() {
+    alert('Con el codigo: JT05 obtendrás un 5% en tu proxima compra');
+  }
